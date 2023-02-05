bb7e74f322d251fce1d09c52051c58ea07b197d6
SHJang <jshh718@naver.com>
2023-02-05 17:55:30 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TCPSocket
+
+Developed with Unreal Engine 5
---
+++ Source/TCPSocket/TCPSocketGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TCPSocketGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TCPSOCKET_API ATCPSocketGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
